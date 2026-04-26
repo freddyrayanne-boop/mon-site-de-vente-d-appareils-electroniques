1d60a58c82b4485d5858e69302367b8351a848b1
freddyrayanne-boop <freddyrayanne@gmail.com>
2026-04-26 14:34:29 +0100

    ajout des fichiers

--- PROJET/freddy3.php
+++ PROJET/freddy3.php
@@ -20,7 +20,7 @@ include 'db.php';
 <div class="brand-wrapper">
 <div class="scroll-container">
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/apple.jpg" alt="APPLE">
+                <img src="images/apple.jpg" alt="APPLE">
                 <p>APPLE</p>
             </div>
             <div class="brand-card">
@@ -28,44 +28,44 @@ include 'db.php';
                 $reqIphone=$pdo->query("SELECT * FROM produits WHERE nom LIKE 'iphone%'");
                 while ($p =$reqIphone->fetch()) : ?>
                 <div class="produit-card">
-                    <img src="images/<?php echo $p ['iphone_11.jpg']; ?>" alt="iphone 11">
-                    <h3><?php echo $p ['iphone 11']; ?></h3>
-                    <p class="prix"><?php echo number_format( float $number , int $decimals = 0 ,['prix'], 0,) </p>
+                    <img src="images/<?php echo $p ['image_url']; ?>" alt="iphone">
+                    <h3><?php echo $p ['nom']; ?></h3>
+                    <p class="prix"><?php echo number_format ($p['prix'], 0, ',',' '); ?> </p>
                     <?php endwhile; ?>
                 </div>
                 <img src="C:/xampp/htdocs/images/samsung.jpg" alt="SAMSUNG">
                 <p>SAMSUNG</p>
             </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/xiaomi.jpg" alt="XIAOMI">
+                <img src="images/xiaomi.jpg" alt="XIAOMI">
                 <p>XIAOMI</p>
             </div>
                 <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/pixel.jpg" alt="pixel">
+                <img src="images/pixel.jpg" alt="pixel">
                 <p>GOOGLE PIXEL</p>
             </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/infinix.jpg" alt="INFINIX">
+                <img src="images/infinix.jpg" alt="INFINIX">
                 <p>INFINIX</p>
             </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/huawei.jpg" alt="HUAWEI">
+                <img src="images/huawei.jpg" alt="HUAWEI">
                 <p>HUAWEI</p>
             </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/techno.jpg" alt="TECHNO">
+                <img src="images/techno.jpg" alt="TECHNO">
                 <p>TECNO</p>
             </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/itel.jpg" alt="ITEL">
+                <img src="images/itel.jpg" alt="ITEL">
                 <p>ITEL</p>
             </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/vivo.jpg" alt="VIVO">
+                <img src="images/vivo.jpg" alt="VIVO">
                 <p>VIVO</p>
             </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/honor.jpg" alt="HONOR">
+                <img src="simages/honor.jpg" alt="HONOR">
                 <p>HONOR</p>
             </div>
             </div>
@@ -76,23 +76,23 @@ include 'db.php';
                 <h2>NOS ORDINATEURS</h2>
                 <div class="container-grid">
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/apple.jpg" alt="APPLE">
+                <img src="images/apple.jpg" alt="APPLE">
                 <p>APPLE</p>
                 </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/dell.jpg" alt="DELL">
+                <img src="images/dell.jpg" alt="DELL">
                 <p>DELL</p>
                 </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/hp.jpg" alt="HP">
+                <img src="images/hp.jpg" alt="HP">
                 <p>HP</p>
                 </div>
             <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/intel.jpg" alt="INTEL">
+                <img src="images/intel.jpg" alt="INTEL">
                 <p>INTEL</p>
                 </div>
                 <div class="brand-card">
-                <img src="C:/xampp/htdocs/images/lenovo.jpg" alt="LENOVO">
+                <img src="images/lenovo.jpg" alt="LENOVO">
                 <p>LENOVO</p>
             </div>
             </div>
